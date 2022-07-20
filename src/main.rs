1934d7b92be28ae6463e281ad4fe6adb2b54e094
Gaurav-Lab-PC <atreyagv@mail.uc.edu>
2022-07-20 16:29:27 -0400

    Show completed time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
+use chrono::{Local};
 use clap::Parser;
 
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
@@ -20,12 +21,15 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    println!("{}", &args.path);
+    println!("Waiting Signals on: {}", &args.path);
     let m = Arc::new(MultiProgress::new());
-    let sty = ProgressStyle::default_bar().template("{bar:50.white/yellow} {pos:>7}/{len:7}");
-    
+    let sty = ProgressStyle::default_bar().template(
+	&format!("{}{}{}","{prefix:10} [{percent:>3.green}] {bar:", args.length,
+		 "} {pos:>7}/{len:7} {eta} {msg}"));
+
     let pb = m.add(ProgressBar::new(args.process_count));
     pb.set_style(sty.clone());
+    pb.set_prefix("Total");
 
     let m2 = m.clone();
     let _ = thread::spawn(move || {
@@ -35,14 +39,17 @@ fn main() {
         for _ in 0..args.process_count {
             let pb2 = m2.add(ProgressBar::new(128));
             pb2.set_style(sty.clone());
-            for _ in 0..128 { 
+	    pb2.set_prefix("Subprocess");
+            for _ in 0..128 {
                 pb2.inc(1);
                 thread::sleep(Duration::from_millis(5));
             }
-            pb2.finish_with_message("done");
+	    let now = Local::now();
+            pb2.finish_with_message(format!("done {}", now.format("%m/%d %H:%M:%S")));
             pb.inc(1);
         }
-        pb.finish_with_message("done");
+	let now = Local::now();
+        pb.finish_with_message(format!("done {}", now.format("%m/%d %H:%M:%S")));
     });
 
     m.join().unwrap();
